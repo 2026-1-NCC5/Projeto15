9b62cdefe21021135535db391a6e2a6f62176985
LucasSilvaMaciel <163528966+LucasSilvaMaciel@users.noreply.github.com>
2026-03-02 23:58:44 -0300

    primeiro commit
    xml feitos

---
+++ LEContagem/app/src/androidTest/java/com/example/lecontagem/LoginActivity.java
@@ -0,0 +1,4 @@
+package com.example.lecontagem;
+
+public class LoginActivity {
+}
---
+++ LEContagem/app/src/main/java/com/example/lecontagem/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.example.lecontagem;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+}
